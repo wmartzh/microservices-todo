bf71292c12b879b09d624a696f389fe018a0e749
wmartzh <wmartzh@gmai.com>
2022-04-26 00:12:32 -0600

    add connections

--- todos/server.ts
+++ todos/server.ts
@@ -1,8 +1,13 @@
 import express, { Application } from "express";
-
+import mongoose from "mongoose"
 import * as http from "http";
 
+const {DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD} = process.env;
+
+const DB_URL = `mongodb://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}`
+
 const app = express();
+
 
 export default class Server {
   router(routes: (app: Application) => void) {
@@ -10,8 +15,18 @@ export default class Server {
     return this;
   }
   listen(port: number): Application {
-    http.createServer(app).listen(port, () => {
-      console.log(`Server running at port:${port}`);
+    http.createServer(app).listen(port,  async () => {
+      try {
+        await mongoose.connect(DB_URL, {
+            seNewUrlParser: true, 
+            useUnifiedTopology: true
+        })
+        console.log("Database connected")
+        console.log(`Server running at port:${port}`);
+        
+      } catch (error) {
+        console.error(error)
+      }
     });
     return app;
   }
